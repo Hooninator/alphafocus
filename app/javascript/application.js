265e1cb53c5d9c0b52114e4fba9da7552868791b
Hooninator <jbellavita@berkeley.edu>
2021-12-28 18:33:22 -0800

    added bootstrap and jquery

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,8 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+import { Application } from "stimulus"
+import { definitionsFromContext } from "stimulus/webpack-helpers"
+
+import "bootstrap"
+import "../stylesheets/application"
